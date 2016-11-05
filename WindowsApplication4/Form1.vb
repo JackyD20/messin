96bce899a9f1c2081009bb1061b2ddecf3f54592
Jackson Densham <jacksonrocks@hotmail.ca>
2016-11-05 17:37:02 -0400

    Add project files.

---
+++ WindowsApplication4/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String = Nothing
+        name = Interaction.InputBox("what is your name?")
+        MessageBox.Show("hello, " & name)
+
+    End Sub
+End Class
